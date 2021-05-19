6f75df6aaccb04f9b5ae97ae92bb10ebe6cde80f
Saurav <>
2021-05-19 04:10:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+#defining our host
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #translating hostname to IPv4
+else:
+	print("Invalid amount of arguments.\n Syntax: python3 scanner.py <ip>")
+
+#banner
+print("-" * 50)
+print("Scanning target IP" + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port))
+
+		if result == 0:
+			print('Port {} is open.'.format(port))
+			s.close()
+
+except KeyboardInterrupt:
+	print('\nExiting Program.')
+	sys.exit()
+
+except socket.gaierror:
+	print('Host could not be resolved.')
+	sys.exit()
+
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
